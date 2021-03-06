186120dbbfee3491acbc880e3b471cff79279c1e
Bradley Bellairs <BWBellairs@outlook.com>
2021-03-06 18:48:39 +0000

    Clean-up

--- sketches/master.c
+++ sketches/master.c
@@ -1,12 +1,13 @@
 int MASTER_START_PIN = 3;
 int TRIGGER_PIN = 13;
 int ECHO_PIN = A0;
-int PULSE_DELAY = 100;
 
 int missed = 0; // Number of marbles the player has failed to catch
 int failCount = 3; // Number of marbles missed before game ends
 
 int inProgress = false; // Game in progress
+
+float lastMarbleDistance = 0; // Normal distance to the wall, used for comparison to detect marbles
 
 void setup() {
   pinMode(MASTER_START_PIN, OUTPUT);
@@ -14,6 +15,7 @@ void setup() {
   pinMode(TRIGGER_PIN, OUTPUT);
   Serial.begin(9600);
   digitalWrite(MASTER_START_PIN, HIGH);
+ 
 }
 
 float getDistance() {
@@ -26,37 +28,23 @@ float getDistance() {
   
   // Read the duraton of the sound wave
   int duration = pulseIn(ECHO_PIN, HIGH);
-  //Serial.println("duration");
-  //Serial.print(duration);
   float distance = duration * 0.034 / 2;
-  //Serial.println(distance);
-  //Serial.println(int(distance));
   return distance;
 }
 
-// The distance measured from the senor to the wall
-// as a way of figuring out whena marble has passed
-//Serial.println("distance to wall");
-int lastMarbleDistance = 0;
-//Serial.println(lastMarbleDistance);
 void loop()
 {
   if (lastMarbleDistance == 0) {
-    lastMarbleDistance = getDistance();
+  	lastMarbleDistance = getDistance();  
   }
   
-  int currentDistance = getDistance();
-  Serial.println(currentDistance);
+  float currentDistance = getDistance();
   if (currentDistance + 2 < lastMarbleDistance) {
-   // a marble has passed
     missed++;
-    Serial.println("a marble has been missed!");
-
   }
 
   if (missed == failCount) {
   	digitalWrite(MASTER_START_PIN, LOW); 
-    Serial.println("game over");
   }
   
   delay(200);
