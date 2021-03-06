9ad4bef524572a736c244d706ba5a8c5d4bbab6e
Bradley Bellairs <BWBellairs@outlook.com>
2021-03-06 20:02:27 +0000

    Added neopixel controller and piezo

--- sketches/master.c
+++ sketches/master.c
@@ -1,6 +1,11 @@
+#include <Adafruit_NeoPixel.h>
+
 int MASTER_START_PIN = 3;
 int TRIGGER_PIN = 13;
 int ECHO_PIN = A0;
+int START_BUTTON_PIN = A1;
+int PEIZO_PIN = 12;
+int NEOPIXEL_PIN = 2;
 
 int missed = 0; // Number of marbles the player has failed to catch
 int failCount = 3; // Number of marbles missed before game ends
@@ -9,13 +14,18 @@ int inProgress = false; // Game in progress
 
 float lastMarbleDistance = 0; // Normal distance to the wall, used for comparison to detect marbles
 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(20, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+uint32_t red = strip.Color(255, 0, 0);
+
 void setup() {
   pinMode(MASTER_START_PIN, OUTPUT);
+  pinMode(START_BUTTON_PIN, INPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(PEIZO_PIN, OUTPUT);
   Serial.begin(9600);
-  digitalWrite(MASTER_START_PIN, HIGH);
- 
+  
+  digitalWrite(MASTER_START_PIN, LOW);
 }
 
 float getDistance() {
@@ -38,13 +48,33 @@ void loop()
   	lastMarbleDistance = getDistance();  
   }
   
+  if (!inProgress && digitalRead(START_BUTTON_PIN) == LOW) {
+   inProgress = true; 
+   digitalWrite(MASTER_START_PIN, HIGH);
+  }
+  
   float currentDistance = getDistance();
   if (currentDistance + 2 < lastMarbleDistance) {
     missed++;
   }
 
-  if (missed == failCount) {
-  	digitalWrite(MASTER_START_PIN, LOW); 
+  if (missed == failCount && inProgress) {
+  	digitalWrite(MASTER_START_PIN, LOW);
+    inProgress = false;
+    digitalWrite(PEIZO_PIN, HIGH);
+    strip.fill(red, 0, 20);
+    strip.show();
+    delay(100);
+    strip.fill(0, 0, 20);
+    strip.show();
+    digitalWrite(PEIZO_PIN, LOW);
+    delay(100);
+    strip.fill(red, 0, 20);
+    strip.show();
+    delay(100);
+    strip.fill(0, 0, 20);
+    strip.show();
+    missed = 0;
   }
   
   delay(200);
